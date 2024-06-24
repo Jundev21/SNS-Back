0980f2cee80d7d43307403e4873b1e41ac3f0cdf
jun <jundev21@gmail.com>
2024-06-24 23:36:06 +0900

    [update] 멤버 서비스 리팩토링

--- src/main/java/com/sns/sns/service/domain/board/controller/BoardController.java
+++ src/main/java/com/sns/sns/service/domain/board/controller/BoardController.java
@@ -1,75 +1,84 @@
 package com.sns.sns.service.domain.board.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.sns.sns.service.common.response.Response;
+import com.sns.sns.service.common.basicResponse.DataResponse;
 import com.sns.sns.service.domain.board.dto.request.BoardRequest;
 import com.sns.sns.service.domain.board.dto.request.BoardUpdateRequest;
-import com.sns.sns.service.domain.board.dto.response.*;
+import com.sns.sns.service.domain.board.dto.response.BoardDeleteResponse;
+import com.sns.sns.service.domain.board.dto.response.BoardDetailResponse;
+import com.sns.sns.service.domain.board.dto.response.BoardGetResponse;
+import com.sns.sns.service.domain.board.dto.response.BoardResponse;
+import com.sns.sns.service.domain.board.dto.response.BoardUpdateResponse;
 import com.sns.sns.service.domain.board.service.BoardService;
 import com.sns.sns.service.domain.member.model.entity.Member;
-import lombok.RequiredArgsConstructor;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
 
-import org.springframework.data.domain.Pageable;
-import java.util.List;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/board")
 public class BoardController {
 
-    private final BoardService  boardService;
+	private final BoardService boardService;
 
-    @PostMapping
-    public Response<BoardResponse> writeBoard(
-            @RequestBody BoardRequest boardRequest,
-            @AuthenticationPrincipal Member member
-            ){
-        return Response.success(boardService.writeBoard(boardRequest,member));
-    }
+	@PostMapping
+	public DataResponse<BoardResponse> writeBoard(
+		@RequestBody @Valid BoardRequest boardRequest,
+		@AuthenticationPrincipal Member member
+	) {
+		return DataResponse.successBodyResponse(HttpStatus.OK, boardService.writeBoard(boardRequest, member));
+	}
 
-    @PutMapping("/{boardId}")
-    public Response<BoardUpdateResponse> updateBoard(
-            @PathVariable Long boardId,
-            @RequestBody BoardUpdateRequest boardUpdateRequest,
-            @AuthenticationPrincipal Member member
-    ){
-        return Response.success(boardService.updateBoard(boardId, boardUpdateRequest,member));
-    }
+	@PutMapping("/{boardId}")
+	public DataResponse<BoardUpdateResponse> updateBoard(
+		@PathVariable Long boardId,
+		@RequestBody @Valid BoardUpdateRequest boardUpdateRequest,
+		@AuthenticationPrincipal Member member
+	) {
+		return DataResponse.successBodyResponse(HttpStatus.OK,
+			boardService.updateBoard(boardId, boardUpdateRequest, member));
+	}
 
-    @DeleteMapping("/{boardId}")
-    public Response<BoardDeleteResponse> deleteBoard(
-            @PathVariable Long boardId,
-            @AuthenticationPrincipal Member member
-    ){
-        return Response.success(boardService.deleteBoard(boardId,member));
-    }
+	@DeleteMapping("/{boardId}")
+	public DataResponse<BoardDeleteResponse> deleteBoard(
+		@PathVariable Long boardId,
+		@AuthenticationPrincipal Member member
+	) {
+		return DataResponse.successBodyResponse(HttpStatus.OK, boardService.deleteBoard(boardId, member));
+	}
 
-    @GetMapping("/{boardId}")
-    public Response<BoardDetailResponse> deleteBoard(
-            @PathVariable Long boardId
-    ){
-        return Response.success(boardService.getBoardDetail(boardId));
-    }
+	@GetMapping("/{boardId}")
+	public DataResponse<BoardDetailResponse> deleteBoard(
+		@PathVariable Long boardId
+	) {
+		return DataResponse.successBodyResponse(HttpStatus.OK, boardService.getBoardDetail(boardId));
+	}
 
-    @GetMapping
-    public Response<Page<BoardGetResponse>> getBoard(
-            Pageable pageable
-    ){
-        return Response.success(boardService.getBoard(pageable));
-    }
-    @GetMapping("/user")
-    public Response<Page<BoardGetResponse>> getUserBoard(
-            Pageable pageable, @AuthenticationPrincipal Member member
-    ){
-        return Response.success(boardService.getUserBoard(pageable, member));
-    }
+	@GetMapping
+	public DataResponse<Page<BoardGetResponse>> getBoard(
+		Pageable pageable
+	) {
+		return DataResponse.successBodyResponse(HttpStatus.OK, boardService.getBoard(pageable));
+	}
 
-
+	@GetMapping("/user")
+	public DataResponse<Page<BoardGetResponse>> getUserBoard(
+		Pageable pageable, @AuthenticationPrincipal Member member
+	) {
+		return DataResponse.successBodyResponse(HttpStatus.OK, boardService.getUserBoard(pageable, member));
+	}
 
 }
--- src/main/java/com/sns/sns/service/domain/board/service/BoardService.java
+++ src/main/java/com/sns/sns/service/domain/board/service/BoardService.java
@@ -1,94 +1,99 @@
 package com.sns.sns.service.domain.board.service;
 
-
-import com.sns.sns.service.domain.board.dto.request.BoardRequest;
-import com.sns.sns.service.domain.board.dto.request.BoardUpdateRequest;
-import com.sns.sns.service.domain.board.dto.response.*;
-import com.sns.sns.service.domain.board.model.BoardEntity;
-import com.sns.sns.service.domain.board.repository.BoardRepository;
-import com.sns.sns.service.domain.exception.BasicException;
-import com.sns.sns.service.domain.exception.ErrorCode;
-import com.sns.sns.service.domain.member.model.entity.Member;
-import com.sns.sns.service.domain.member.repository.MemberRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.sns.sns.service.common.exception.BasicException;
+import com.sns.sns.service.common.exception.ErrorCode;
+import com.sns.sns.service.domain.board.dto.request.BoardRequest;
+import com.sns.sns.service.domain.board.dto.request.BoardUpdateRequest;
+import com.sns.sns.service.domain.board.dto.response.BoardDeleteResponse;
+import com.sns.sns.service.domain.board.dto.response.BoardDetailResponse;
+import com.sns.sns.service.domain.board.dto.response.BoardGetResponse;
+import com.sns.sns.service.domain.board.dto.response.BoardResponse;
+import com.sns.sns.service.domain.board.dto.response.BoardUpdateResponse;
+import com.sns.sns.service.domain.board.model.BoardEntity;
+import com.sns.sns.service.domain.board.repository.BoardRepository;
+import com.sns.sns.service.domain.member.model.entity.Member;
+import com.sns.sns.service.domain.member.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
 @RequiredArgsConstructor
 public class BoardService {
 
-    private final MemberRepository memberRepository;
-    private final BoardRepository boardRepository;
-    
-    @Transactional
-    public BoardResponse writeBoard(BoardRequest boardRequest, Member member) {
+	private final MemberRepository memberRepository;
+	private final BoardRepository boardRepository;
 
-        Member findMember = getMember(member);
-        BoardEntity board = new BoardEntity(boardRequest.title(), boardRequest.contents(), member);
-        BoardEntity savedBoard = boardRepository.save(board);
-        return BoardResponse.boardResponse(savedBoard, member);
-    }
+	@Transactional
+	public BoardResponse writeBoard(BoardRequest boardRequest, Member member) {
+		checkExistMember(member);
+		BoardEntity newBoard = new BoardEntity(boardRequest.title(), boardRequest.contents(), member);
+		BoardEntity savedBoard = boardRepository.save(newBoard);
+		return BoardResponse.boardResponse(savedBoard, member);
+	}
 
-    @Transactional
-    public BoardUpdateResponse updateBoard(Long boardId, BoardUpdateRequest boardUpdateRequest, Member member) {
+	@Transactional
+	public BoardUpdateResponse updateBoard(Long boardId, BoardUpdateRequest boardUpdateRequest, Member member) {
+		Member findMember = checkExistMember(member);
+		BoardEntity findBoard = getBoardEntity(boardId);
+		checkUpdatePermission(member, findBoard);
+		BoardEntity updatedBoard = findBoard.updateBoard(boardUpdateRequest);
+		return BoardUpdateResponse.boardUpdateResponse(updatedBoard, findMember);
+	}
 
-        Member findMember = getMember(member);
-        BoardEntity board = getBoardEntity(boardId);
+	@Transactional
+	public BoardDeleteResponse deleteBoard(Long boardId, Member member) {
+		Member findMember = checkExistMember(member);
+		BoardEntity findBoard = getBoardEntity(boardId);
+		checkUpdatePermission(member, findBoard);
+		boardRepository.delete(findBoard);
+		return BoardDeleteResponse.boardDeleteResponse(findBoard, findMember);
+	}
 
-        if(findMember != board.getMember()){
-            throw new BasicException(ErrorCode.INVALID_PERMISSION,ErrorCode.INVALID_PERMISSION.getMessage());
-        }
+	@Transactional(readOnly = true)
+	public Page<BoardGetResponse> getBoard(Pageable pageable) {
+		Page<BoardEntity> board = boardRepository.findAll(pageable);
+		//        Page<BoardEntity> board = boardRepository.findAllBoard(pageable);
+		return board.map(BoardGetResponse::boardGetResponse);
+	}
 
-        BoardEntity newBoard = board.updateBoard(boardUpdateRequest);
-        return BoardUpdateResponse.boardUpdateResponse(newBoard, findMember);
-    }
+	@Transactional(readOnly = true)
+	public Page<BoardGetResponse> getUserBoard(Pageable pageable, Member member) {
+		Member findMember = memberRepository.findByUserName(member.getUserLoginId())
+			.orElseThrow(() -> new BasicException(ErrorCode.NOT_EXIST_MEMBER, ErrorCode.NOT_EXIST_MEMBER.getMsg()));
 
-    @Transactional
-    public BoardDeleteResponse deleteBoard(Long boardId, Member member) {
+		Page<BoardEntity> boardEntities = boardRepository.findAllByMember(findMember, pageable);
+		return boardEntities.map(BoardGetResponse::boardGetResponse);
 
-        Member findMember = getMember(member);
-        BoardEntity board = getBoardEntity(boardId);
+	}
 
-        if(findMember != board.getMember()){
-            throw new BasicException(ErrorCode.INVALID_PERMISSION,ErrorCode.INVALID_PERMISSION.getMessage());
-        }
+	@Transactional(readOnly = true)
+	public BoardDetailResponse getBoardDetail(Long boardId) {
+		BoardEntity findBoard = getBoardEntity(boardId);
+		return BoardDetailResponse.boardDetailResponse(findBoard);
+	}
 
-        boardRepository.delete(board);
-        return BoardDeleteResponse.boardDeleteResponse(board, findMember);
-    }
-    @Transactional (readOnly = true)
-    public Page<BoardGetResponse> getBoard(Pageable pageable) {
-        Page<BoardEntity> board = boardRepository.findAll(pageable);
-//        Page<BoardEntity> board = boardRepository.findAllBoard(pageable);
-        return board.map(BoardGetResponse::boardGetResponse);
-    }
-    @Transactional (readOnly = true)
-    public Page<BoardGetResponse> getUserBoard(Pageable pageable, Member member) {
-        Member findMember = memberRepository.findByUserName(member.getUsername())
-                .orElseThrow(() -> new BasicException(ErrorCode.NOT_EXIST_MEMBER, ErrorCode.NOT_EXIST_MEMBER.getMessage()));
+	@Transactional(readOnly = true)
+	private Member checkExistMember(Member member) {
+		return memberRepository.findByUserName(member.getUserLoginId())
+			.orElseThrow(() -> new BasicException(ErrorCode.NOT_EXIST_MEMBER, ErrorCode.NOT_EXIST_MEMBER.getMsg()));
 
-        Page<BoardEntity> boardEntities = boardRepository.findAllByMember(findMember, pageable);
-        return boardEntities.map(BoardGetResponse::boardGetResponse);
-        
-    }
+	}
 
-    @Transactional(readOnly = true)
-    public BoardDetailResponse getBoardDetail(Long boardId) {
-        BoardEntity findBoard = getBoardEntity(boardId);
-        return BoardDetailResponse.boardDetailResponse(findBoard);
-    }
+	@Transactional(readOnly = true)
+	private BoardEntity getBoardEntity(Long boardId) {
+		return boardRepository.findById(boardId)
+			.orElseThrow(() -> new BasicException(ErrorCode.NOT_EXIST_BOARD, ErrorCode.NOT_EXIST_BOARD.getMsg()));
+	}
 
-    private Member getMember(Member member) {
-        Member findMember = memberRepository.findByUserName(member.getUsername())
-                .orElseThrow(() -> new BasicException(ErrorCode.NOT_EXIST_MEMBER, ErrorCode.NOT_EXIST_MEMBER.getMessage()));
-        return findMember;
-    }
-    private BoardEntity getBoardEntity(Long boardId) {
-        BoardEntity board = boardRepository.findById(boardId)
-                .orElseThrow(()->new BasicException(ErrorCode.NOT_EXIST_BOARD,ErrorCode.NOT_EXIST_BOARD.getMessage()));
-        return board;
-    }
+	@Transactional(readOnly = true)
+	private void checkUpdatePermission(Member member, BoardEntity board) {
+		if (!member.getUserLoginId().equals(board.getMember().getUserLoginId())) {
+			throw new BasicException(ErrorCode.INVALID_PERMISSION, ErrorCode.INVALID_PERMISSION.getMsg());
+		}
+	}
 }
